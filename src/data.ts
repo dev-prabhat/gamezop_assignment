4016a53db92bb143b83c20acb3b0d8228e0b1031
dev-prabhat <prabhatsingh55rocks@gmail.com>
2024-08-02 17:31:05 +0530

    feat: data for local text and images

---
+++ src/data.ts
@@ -0,0 +1,64 @@
+import { imagesAnimationType } from "./types";
+
+export const imagesAnimation: imagesAnimationType = {
+  'slides1': [
+    "https://static.gamezop.com/rkAXTzkD5kX/square.png",
+    "https://static.gamezop.com/ByQxJnp7qRB/square.png",
+    "https://static.gamezop.com/HJXei0j/square.png",
+    "https://static.gamezop.com/HJP4afkvqJQ/square.png",
+  ],
+  'slides2': [
+    "https://static.gamezop.com/rkAXTzkD5kX/square.png",
+    "https://static.gamezop.com/ByQxJnp7qRB/square.png",
+    "https://static.gamezop.com/HJXei0j/square.png",
+    "https://static.gamezop.com/HJP4afkvqJQ/square.png",
+  ],
+};
+
+export const footerLinks = [
+    {
+      name: "Terms of Use",
+      src: "https://static.gamezop.com/legal/terms-of-use/",
+    },
+    {
+      name: "Privacy Policy",
+      src: "https://static.gamezop.com/legal/privacy/",
+    },
+    {
+      name: "About",
+      src: "https://business.gamezop.com/about-us",
+    },
+    {
+      name: "Jobs",
+      src: "https://wellfound.com/company/gamezop/jobs",
+    },
+    {
+      name: "Partner With Us",
+      src: "https://business.gamezop.com/",
+    },
+  ];
+  
+export const footerDescriptionText = [
+    {
+      text: "Gamezop is a plug-and-play gaming platform that any app or website can integrate to bring casual gaming for its users. Gamezop also operates Quizzop, a quizzing platform, that digital products can add as a trivia section.",
+    },
+    {
+      text: "Over 5,000 products from more than 70 countries have integrated Gamezop and Quizzop. These include Amazon, Samsung Internet, Snap, Tata Play, AccuWeather, Paytm, Gulf News, and Branch.",
+    },
+    {
+      text: "Games and trivia increase user engagement significantly within all kinds of apps and websites, besides opening a new stream of advertising revenue. Gamezop and Quizzop take 30 minutes to integrate and can be used for free: both by the products integrating them and end users",
+    },
+    {
+      text: "If you wish to add games and trivia to your product, write to us on:",
+      src: "mailto:partnerships@gamezop.com",
+      linkText: "partnerships@gamezop.com",
+    },
+    {
+      text: "For more information, please visit:",
+      src: "https://business.gamezop.com/",
+      linkText: "business.gamezop.com",
+    },
+    {
+      text: '© 2024 Advergame Technologies Pvt. Ltd. ("ATPL"). Gamezop ® & Quizzop ® are registered trademarks of ATPL. Property ID: cfuucl7YgA',
+    },
+  ];
