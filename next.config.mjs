a3968d84b88ccc9de726dc96d0a4b7e03b4e3a79
dev-prabhat <prabhatsingh55rocks@gmail.com>
2024-08-03 19:30:35 +0530

    feat: Added PWA in app

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,27 @@
+import nextPWA from 'next-pwa';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development',
+  },
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "static.gamezop.com",
+        protocol: 'https',
+        hostname: 'static.gamezop.com',
       },
     ],
   },
 };
 
-export default nextConfig;
+const withPWA = nextPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
+export default withPWA(nextConfig);
